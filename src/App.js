30f53d70ef6b4a4e5cba2006f6eaa7bf0198ffca
PAUL <you@example.com>
2022-12-14 21:58:29 +0300

    adding the add description

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import { useState} from "react"
 function App() {
   const [todoList, setTodoList] = useState([])
   const [inputTask, setInputTask] = useState("")
+  const [inputDescriptionTask, setInputDescriptionTask] = useState("")
 
 
   const handleInput = (event) => {
     setInputTask(event.target.value)
+    
+  }
+  const handleInputDescription = (event) => {
+    setInputDescriptionTask(event.target.value)
+    
   }
 
   const saveInput = () => {
@@ -18,13 +24,32 @@ function App() {
     const task =  {
       id: todoList.length === 0 ? 1: todoList[todoList.length -1].id + 1,
       taskName: inputTask,
+      taskDescription: inputDescriptionTask,
+      complete: false,
     }
 
     setTodoList([...todoList, task])
   }
+  
+
+  const completeHandler = (id) => {
+    
+    setTodoList(todoList.map((task) => {
+      if (task.id === id ){
+        return {...task, complete:true};
+        
+      }else {
+        return task;
+      }
+    }))
+
+
+  }  
 
   const deleteHandler = (id) => {
     const newTodoList = todoList.filter((task) => task.id !== id)
+    console.log(todoList.filter((task) => task.id !== id))
+
 
     setTodoList(newTodoList)
 
@@ -37,18 +62,53 @@ function App() {
 
     
       <div className="addTask">
-        <input type="text" value={inputTask} onChange={handleInput} />
+        <input type="text" value={inputTask} onChange={handleInput} />       
+
 
         <button onClick={saveInput} >Add Task</button>
+
+        <div className='addDesctiption'>
+        <input type="text" value={inputDescriptionTask} onChange={handleInputDescription} />
+        </div>
         
+        
+        
+
       </div>
-      <div className="addTask">
+
+
+
+      <div className="displayTask">
 
         {todoList.map((task, hey) => {
           return (
-            <div>
-              <h4 key={hey}>{task.taskName} </h4>
-              <button onClick={ () => deleteHandler(task.id)}>X</button>
+            <div key={hey}>
+            
+
+              {task.complete === false && 
+              <div>
+              <h4 key={hey}>{task.id}  {task.taskName}  </h4> 
+              <p>
+                {task.taskDescription}
+              </p>
+              </div>
+              
+              
+               }
+              
+              
+              {task.complete === true && <div>
+                <h4 key={hey}>{task.id}  {task.taskName} </h4> 
+                <p> {task.taskDescription} </p>
+                <p> -Complete</p>
+              </div> }
+
+
+              <button style={{ backgroundColor: "green", borderColor: 'green', }} onClick={ () => completeHandler(task.id)}>Complete</button> 
+
+              <button onClick={ () => deleteHandler(task.id)}> Delete </button> 
+              
+              
             </div>
           )
         })}
@@ -56,6 +116,10 @@ function App() {
 
     </div>
   );
+
+
+
+
 }
 
 export default App;
